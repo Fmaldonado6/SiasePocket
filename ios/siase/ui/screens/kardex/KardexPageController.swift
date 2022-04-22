5cbd071f3b65ff1cd6bcd2968b1446db5ff397ec
Fernando Maldonado <Fmaldonado824@gmail.com>
2022-04-22 16:59:08 -0500

    Registered carded in DI

--- ios/siase/ui/screens/kardex/KardexPageController.swift
+++ ios/siase/ui/screens/kardex/KardexPageController.swift
@@ -18,12 +18,48 @@ class KardexPageController : UIViewController{
         return view
     }()
     
+    private lazy var tableView:UITableView = {
+        let view = UITableView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.delegate = self
+        view.dataSource = self
+        return view
+    }()
+    
+    private var subjects:[[Subject]] = [[Subject]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGroupedBackground
         navigationItem.title = "Kárdex"
         navigationController?.navigationBar.prefersLargeTitles = true
-        navigationItem.largeTitleDisplayMode = .automatic;
+        navigationItem.largeTitleDisplayMode = .never;
+        setupViews()
     }
+    
+    private func setupViews(){
+        view.addSubview(segmentedControl)
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            segmentedControl.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            segmentedControl.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        ])
+    }
+    
+}
+
+extension KardexPageController: UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return subjects.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "ASDASD"
+        return cell
+    }
+    
     
 }
--- ios/siase/ui/screens/kardex/KardexPageViewModel.swift
+++ ios/siase/ui/screens/kardex/KardexPageViewModel.swift
@@ -9,6 +9,13 @@ import Foundation
 
 class KardexPageViewModel:BaseViewModel{
     
+    private let kardexRepository:KardexRepository
     
+    init(
+        authRepository: AuthRepository = DIContainer.shared.resolve(type: AuthRepository.self)!,
+        kardexRepository:KardexRepository = DIContainer.shared.resolve(type: KardexRepository.self)!
+    ) {
+        self.kardexRepository = kardexRepository
+    }
     
 }
