bbedb50d454de4fe68cb60226ad8d0d6e8d2045a
Fernando Maldonado <Fmaldonado824@gmail.com>
2022-04-22 16:43:57 -0500

    Added helpers

---
+++ ios/siase/ui/utils/Helpers.swift
@@ -0,0 +1,21 @@
+//
+//  Helpers.swift
+//  siase
+//
+//  Created by Fernando Maldonado on 22/04/22.
+//
+
+import Foundation
+
+class DateHelpers{
+    
+    static let weekDays = [
+        "Lunes",
+        "Martes",
+        "Miércoles",
+        "Jueves",
+        "Viernes",
+        "Sábado"
+    ]
+    
+}
