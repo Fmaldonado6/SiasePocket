8e9ffbeff4c594a3032b7ced627421dfcd26c008
Fernando Maldonado <Fmaldonado824@gmail.com>
2022-04-26 14:08:21 -0500

    Fixed schedules in  iOS

--- ios/siase/data/utils/DataExtensions.swift
+++ ios/siase/data/utils/DataExtensions.swift
@@ -21,11 +21,13 @@ extension Array where Element == ClassDetail {
         var currentSubject = detail.first!
 
         for var classDetail in detail {
-            if (classDetail.claveMateria != currentSubject.claveMateria) {
-                newDetail.append(currentSubject)
-            } else {
+            if (classDetail.claveMateria == currentSubject.claveMateria
+                && currentSubject.horaFin == classDetail.horaInicio
+            ) {
                 classDetail.horaInicio = currentSubject.horaInicio
                 currentSubject.horaFin = classDetail.horaFin
+            } else {
+                newDetail.append(currentSubject)
             }
             currentSubject = classDetail
         }
